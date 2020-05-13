6e2f102283c375b55e2ecb78101e35ab4862b662
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-13 20:07:40 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -498,7 +498,7 @@ def sendBooking(event, user_id):  #房間預約
                     title='房間預約',
                     text='您目前沒有訂房記錄，可以開始預訂房間。',
                     actions=[
-                        URITemplateAction(label='房間預約', uri='https://liff.lineme/.1654180187-Y6trO6gmz')  #開啟LIFF讓使用者輸入訂房資料
+                        URITemplateAction(label='房間預約', uri='https://liff.lineme/1654180187-Y6trO6gmz')  #開啟LIFF讓使用者輸入訂房資料
                     ]
                 )
             )
