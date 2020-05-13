0c89df8f813018849fae6f3d5048ab566fc36088
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-13 19:33:07 +0800

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf.urls import url
 from studentsapp import views
 from django.urls import include
-#from test2api import views as v2
+from hotelapi import views as v2
 from func1api import views as v3
 
 urlpatterns = [
@@ -32,7 +32,7 @@ urlpatterns = [
     url(r'^index/(\w+)/$',views.index),
     url(r'^hello3/(\w+)/$',views.hello3),
     url(r'^callback/', include(('studentsapp.urls', 'studentsapp'), namespace='callback')),
-  #  url(r'^callback', v2.callback),
+    url(r'^callback', v2.callback),
     url(r'^callback', v3.callback),
 ]
 
